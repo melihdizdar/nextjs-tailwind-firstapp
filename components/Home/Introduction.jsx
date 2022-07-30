0f423d1592ba85f19687c426a529b274e16b429a
Melih Dizdar <mlhdzdr@gmail.com>
2022-07-30 22:02:14 +0300

    added layouts

--- components/Home/Introduction.jsx
+++ components/Home/Introduction.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 function Introduction() {
     return (
-        <div className="container mx-auto px-4">
-            Home
+        <div className="container mx-auto px-8 text-center">
+            <div className='flex'>
+                <div className='w-full'>%100</div>
+            </div>
+            <div className='flex'>
+                <div className='w-1/2'>%50</div>
+                <div className='w-1/2'>%50</div>
+            </div>
+            <div className='flex'>
+                <div className='w-1/3'>%33</div>
+                <div className='w-1/3'>%33</div>
+                <div className='w-1/3'>%33</div>
+            </div>
+            <div className='flex'>
+                <div className='w-1/4'>%25</div>
+                <div className='w-1/4'>%25</div>
+                <div className='w-1/4'>%25</div>
+                <div className='w-1/4'>%25</div>
+            </div>
         </div>
     )
 }
